791a49e1084c27e493a9a14d8fde589ca19030da
Veronika <veronikapetrushka@gmail.com>
2024-04-07 19:24:13 +0200

    inner changes

--- src/components/LoadMoreBtn/LoadMoreBtn.jsx
+++ src/components/LoadMoreBtn/LoadMoreBtn.jsx
@@ -1,35 +1,3 @@
-// import { useState } from 'react';
-// import Loader from '../Loader/Loader';
-// import css from './LoadMoreBtn.module.css';
-
-// const LoadMoreBtn = ({ onClick, hasMorePhotos, loading, items }) => {
-//   const [isLoading, setIsLoading] = useState(false);
-
-//   const handleClick = async () => {
-//     setIsLoading(true);
-//     await onClick();
-//     setIsLoading(false);
-//   };
-
-//   return (
-//     <div>
-//       {isLoading || loading ? (
-//         <div style={{ display: 'flex', justifyContent: 'center', marginTop: '20px' }}>
-//           <Loader />
-//         </div>
-//       ) : (
-//         items.length > 0 && hasMorePhotos && (
-//           <button className={css.LoadMoreBtn} onClick={handleClick} style={{ marginTop: '20px' }} disabled={!hasMorePhotos || isLoading || loading}>
-//             Load more
-//           </button>
-//         )
-//       )}
-//     </div>
-//   );
-// };
-
-// export default LoadMoreBtn;
-
 import css from './LoadMoreBtn.module.css';
 
 const LoadMoreBtn = ({ onClick, hasMorePhotos, isLoading }) => {
